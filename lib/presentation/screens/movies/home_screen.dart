f756bd30a9a7c7ed27b5472b092612737f028baa
Silvana Vitali <silgivia@hotmail.com>
2025-05-12 10:25:32 -0400

    feat: add CustomBottomNavigation

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -11,7 +11,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: _HomeView());
+    return Scaffold(
+      body: _HomeView(),
+      bottomNavigationBar: CustomBottomNavigation(),
+    );
   }
 }
 
