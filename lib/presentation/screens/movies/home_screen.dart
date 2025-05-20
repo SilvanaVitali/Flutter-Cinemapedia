89fbbcddf8385f0dbc23ced188c3551bcc8c60fd
Silvana Vitali <silgivia@hotmail.com>
2025-05-20 19:32:27 -0400

    feat: router config

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -5,13 +5,16 @@ import '../../widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
+  final int pageIndex;
 
-  const HomeScreen({super.key});
+  const HomeScreen({super.key, required this.pageIndex});
+
+  final viewRoutes = const <Widget>[HomeView(), SizedBox(), FavoritesView()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: HomeView(),
+      body: IndexedStack(index: pageIndex, children: viewRoutes),
       bottomNavigationBar: CustomBottomNavigation(),
     );
   }
