c97bf6280c3836617006168e059bc5b097af452f
Silvana Vitali <silgivia@hotmail.com>
2025-05-23 14:34:56 -0400

    feat: add show message if there are no favorites

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
+import 'package:go_router/go_router.dart';
 
 //init
 class FavoritesView extends ConsumerStatefulWidget {
@@ -39,6 +40,32 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   @override
   Widget build(BuildContext context) {
     final favoritesMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
+    if (favoritesMovies.isEmpty) {
+      final colors = Theme.of(context).colorScheme;
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.favorite_outline_sharp, size: 60, color: colors.primary),
+            Text(
+              'Ohhh no!!',
+              style: TextStyle(fontSize: 30, color: colors.primary),
+            ),
+            Text(
+              'No tienes películas favoritas',
+              style: TextStyle(fontSize: 20, color: Colors.black45),
+            ),
+            SizedBox(height: 20),
+            FilledButton.tonal(
+              onPressed: () => context.go('/home/0'),
+              child: const Text('Empieza a buscar'),
+            ),
+          ],
+        ),
+      );
+    }
     return Scaffold(
       body: MovieMasonry(loadNextPage: loadNextPage, movies: favoritesMovies),
     );
