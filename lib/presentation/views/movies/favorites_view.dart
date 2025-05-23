66cbb68fa9101cf00d0776d48e6d500dd7034337
Silvana Vitali <silgivia@hotmail.com>
2025-05-23 10:03:33 -0400

    feat: show favoritesMovies

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,13 +1,33 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+//init
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoritesMovies = ref.watch(favoriteMoviesProvider).values.toList();
     return Scaffold(
-      appBar: AppBar(title: Text('Favorites View')),
-      body: const Center(child: Text('Favorites View')),
+      body: ListView.builder(
+        itemCount: favoritesMovies.length,
+        itemBuilder: (contex, index) {
+          final movie = favoritesMovies[index];
+          return ListTile(title: Text(movie.title));
+        },
+      ),
     );
   }
 }
